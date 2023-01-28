52732e7e1d5e8b72fb35fb5050908815d439bf75
mohanadkandil <mohanadmkandil@gmail.com>
2023-01-28 07:37:39 -0800

    chore: add primary color to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#21E6C1",
+      },
+    },
   },
   plugins: [],
 };
