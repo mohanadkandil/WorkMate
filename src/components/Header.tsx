c7ae97b1eecd99991c7c158fe45a1d971235f4d9
mohanadkandil <mohanadmkandil@gmail.com>
2023-02-04 13:04:26 -0800

    style: header colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="mb-9 flex items-center justify-between py-4 px-16">
+    <div className="flex w-full items-center justify-between bg-[#004643] py-4 px-16">
       <Link href={"/"}>
-        <p className="text-3xl font-semibold">
-          Work<span className="text-primary">Mate</span>
+        <p className="text-3xl font-semibold text-white">
+          Work<span className="text-[#f9bc60]">Mate</span>
         </p>
       </Link>
-      <ul className="flex items-center space-x-10 text-base font-semibold">
+      <ul className="flex items-center space-x-10 text-base font-semibold text-white">
         <li>
           <Link href={"/"}>Dashboard</Link>
         </li>
@@ -20,7 +20,7 @@ export default function Header() {
         </li>
         <button
           onClick={() => null}
-          className="rounded-md bg-primary py-1 px-3.5"
+          className="rounded-md bg-white py-1 px-3.5 text-black"
         >
           Logout
         </button>
